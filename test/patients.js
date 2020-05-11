9431b185a219adf50afb1d185f50ec6fba8e13f5
Ambika Kumari <kambika1309@gmail.com>
2020-05-11 23:24:08 +0530

    checked  the newly created patient

--- test/patients.js
+++ test/patients.js
@@ -2,7 +2,9 @@
 process.env.NODE_ENV = 'test';
 
 let mongoose = require("mongoose");
-let Patient = require('../models/patient')
+let Patient = require('../models/patient');
+let Doctor = require('../models/doctor');
+let token='';
 
 //Require the dev-dependencies
 const chai = require('chai');
@@ -11,12 +13,48 @@ const server = require('../index');
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('Patients', () => {
-	beforeEach((done) => { //Before each test we empty the database
-		Patient.remove({}, (err) => { 
-		   done();		   
-		});		
-    });
 
+//so that token can be genrated and used firther
+describe('/login doctors', () => {
+  it('it should check the created doctor logged in', (done) => {
+        chai.request(server)
+        .post('/api/v1/doctors/login')
+        .send({
+            "email":"kambika1309@gmail.com",
+            "password":123
+        })
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.an('object')
+              res.body.should.have.property('token')
+              token = res.body.token
+              
+          done();
+        });
+  });
 });
+
+
+describe('/register patient', () => {
+it('it should return newly created patient', done => {
+  const patient= {
+    mobileNo: 9836531704,
+    name:"Alisha",
+    gender:"Female"
+  }
+ 
+  chai
+   .request(server)
+   .post('/api/v1/patients/register')
+   .set('authorization', `bearer ${token}`)
+   .send(patient)
+   .end((err, res) => {
+    res.should.have.status(200)
+    res.body.should.be.a('object')
+    res.body.should.have.property('message').eql('Patient registered successfully')
+    done()
+   })
+
+  })
+})
 
